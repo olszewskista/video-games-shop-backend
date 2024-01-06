89ee4408d169bd4d2725029c034fc84356f2f161
olszewskista <s.olszewski.408@studms.ug.edu.pl>
2024-01-06 05:08:45 +0100

    fix: hash password only when it is created or edited

--- models/User.js
+++ models/User.js
@@ -85,6 +85,7 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt)
     next()
